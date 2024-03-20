781c28a985663dd68a2d365c0c1b3127f1c7fa26
srvsharma <srvsharma008@gmail.com>
2024-03-20 16:51:56 +0530

    prepare base model completed

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 from src.ChickenDiseaseClassifier import logger
+from src.ChickenDiseaseClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.ChickenDiseaseClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
-logger.info("Welcome to my custom log")
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/ChickenDiseaseClassifier/pipeline/stage_02_prepare_base_model.py
+++ src/ChickenDiseaseClassifier/pipeline/stage_02_prepare_base_model.py
@@ -1,28 +1,30 @@
 from src.ChickenDiseaseClassifier.config.configuration import ConfigurationManager
-from src.ChickenDiseaseClassifier.components.data_ingestion import DataIngestion
+from src.ChickenDiseaseClassifier.components.prepare_base_model import PrepareBaseModel
 from src.ChickenDiseaseClassifier import logger
 
 
-STAGE_NAME = "Data Ingestion stage"
+STAGE_NAME = "Prepare base model"
 
-class DataIngestionTrainingPipeline:
+class PrepareBaseModelTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self):
         config = ConfigurationManager()
-        data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config=data_ingestion_config)
-        data_ingestion.download_file()
-        data_ingestion.extract_zip_file()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
 
 
 
 
 if __name__ == '__main__':
     try:
+        logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
+        obj = PrepareBaseModelTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
